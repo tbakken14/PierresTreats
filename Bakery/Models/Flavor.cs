d422033fbfdbcbe0b47513ac834000608e1323ed
Thomas Bakken <tbakken14@gmail.com>
2023-08-13 10:55:36 -0700

    Add models

---
+++ Bakery/Models/Flavor.cs
@@ -0,0 +1,8 @@
+namespace Bakery.Models
+{
+    public class Flavor
+    {
+        public int FlavorId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Bakery/Models/Treat.cs
@@ -0,0 +1,8 @@
+namespace Bakery.Models
+{
+    public class Treat
+    {
+        public int TreatId { get; set; }
+        public string Name { get; set; }
+    }
+}
